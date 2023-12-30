b8d2079462ddbef07edee525580d6db6682eaf06
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-12-30 14:18:23 +0530

    Implemented Inheritance
    Moved price, title attribute and method get_price() to new class publications

--- inheritance.py
+++ inheritance.py
@@ -1,29 +1,27 @@
-class Book:
+class Publication:
+    def __init__(self, title, price):
+        self.title = title
+        self.price = price
+    def get_price(self):
+        return self.price
+class Book(Publication):
     def __init__(self, title, author, pages, price):
-        self.title = title
+        super().__init__(title, price)
         self.author = author
         self.pages = pages
-        self.price = price
-    def get_price(self):
-        return self.price
 
-class Magazine:
+class Magazine(Publication):
     def __init__(self, title, author, pages, price, period):
-        self.title = title
+        super().__init__(title, price)
         self.author = author
         self.pages = pages
-        self.price = price
         self.period = period
-    def get_price(self):
-        return self.price
-class Newspaper:
+class Newspaper(Publication):
     def __init__(self, title, publisher, price, period):
-        self.title = title
+        super().__init__(title, price)
         self.publisher = publisher
-        self.price = price
         self.period = period
-    def get_price(self):
-        return self.price
+
 B1 = Book("Book 1", "Author 1", 100, 50)
 M1 = Magazine("Magazine 1", "Author 1", 200, 150, 2022)
 N1 = Newspaper("Newzpaper 1", "Media Prints", 200, 2020)
