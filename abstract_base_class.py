11bb9a7284424bcf7245575f1d4fa74f2c31b2bd
Yuvraj198920 <yuvvraaj0flying@gmail.com>
2023-12-30 14:50:17 +0530

    Now every sub-class should implement the base class method calcArea()

--- abstract_base_class.py
+++ abstract_base_class.py
@@ -1,18 +1,29 @@
 from abc import ABC, abstractmethod
+
+
 class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
+
     @abstractmethod
     def calArea(self):
         pass
+
 
 class Circle(GraphicShape):
     def __init__(self, radius: float) -> None:
         self.radius = radius
 
+    def calArea(self):
+        return 3.14 * (self.radius ** 2)
+
+
 class Square(GraphicShape):
     def __init__(self, sides: float):
         self.sides = sides
+
+    def calArea(self):
+        return self.sides * self.sides
 
 
 circ = Circle(10.2)
